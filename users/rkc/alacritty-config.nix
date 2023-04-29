db1605506e8192908bd7dfa41a91e6aa99f60e84
rkc <kumarcrishikesh@gmail.com>
2023-04-29 21:11:53 +0530

    organized config files

---
+++ users/rkc/alacritty-config.nix
@@ -0,0 +1,49 @@
+{
+    font.size = 7.0;	
+    colors = {
+        primary = { 
+        background = "#282a36";
+        foreground = "#f8f8f2";
+        bright_foreground = "#ffffff";
+        };
+        search = {
+        matches.foreground = "#44475a";
+        matches.background = "#50fa7b";
+        focused_match.foreground = "#44475a";
+        focused_match.background ="#ffb86c";
+        };
+        footer_bar = {
+        background = "#282a36";
+        foreground = "#f8f8f2";
+        };
+        hints = {
+        start.foreground = "#282a36";
+        start.background = "#f1fa8c";
+        end.foreground = "#f1fa8c";
+        end.background = "#282a36";
+        };	
+        selection = {
+        background = "#44475a";
+        };
+        normal = { 
+        black = "#21222c";
+        red = "#ff5555";
+        green = "#50fa7b";
+        yellow = "#f1fa8c";
+        blue = "#bd93f9";
+        magenta = "#ff79c6";
+        cyan = "#8be9fd";
+        white = "#f8f8f2";
+        };
+        bright ={
+        black = "#6272a4";
+        red = "#ff6e6e";
+        green = "#69ff94";
+        yellow = "#ffffa5";
+        blue = "#d6acff";
+        magenta = "#ff92df";
+        cyan = "#a4ffff";
+        white = "#ffffff";
+        };
+    };
+}
